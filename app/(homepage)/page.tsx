87f6dc87a2b5bd259e02f8a5393d5d58652eb387
Sampath Bandla <sampath@pixelvide.com>
2023-11-22 18:11:48 +0530

    Header created and gave default for homepage

---
+++ app/(homepage)/page.tsx
@@ -0,0 +1,9 @@
+import Header from "@/components/header";
+
+export default function HomePage() {
+  return (
+	<div>
+		<Header />
+	</div>
+  )
+}
---
+++ components/header.tsx
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <div className="header p-5 whitespace-nowrap">
+		<div className="left">
+			<h3 className="text-xl font-medium">WeBoard</h3>
+		</div>
+		<div className="right">
+			
+		</div>
+    </div>
+  )
+}
